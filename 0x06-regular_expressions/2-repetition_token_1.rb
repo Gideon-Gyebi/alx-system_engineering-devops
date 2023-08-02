c796094030bf2c34b2263831ad6b713b4d9c9c2e
Gideon Gyebi <gyebigideon000@gmail.com>
2023-08-01 22:16:54 -0700

    regular expression mutching staff

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#regular expression that will match some cases
+puts ARGV[0].scan(/htn/).join
+puts ARGV[0].scan(/hbtn/).join
---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#regular expression that will match some cases
+puts ARGV[0].scan(/hbt+n/).join

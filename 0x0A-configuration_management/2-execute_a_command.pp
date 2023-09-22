c36c595392370d423e4a68209f6f8ece6322f3fc
Gideon Gyebi <gyebigideon000@gmail.com>
2023-09-22 16:37:49 -0700

    2. Execute a command

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Using the Puppet to create a manifest that kills a process named killmenow.
+exec { 'killing a process using pkill':
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
+}
